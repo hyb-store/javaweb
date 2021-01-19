0a97728dec72f336d45d6ad69ca071e9bd18609c
hyb-store <3063687049@qq.com>
2021-01-19 19:47:47 +0800

    new file

--- servlet&http&request_test/src/com/hyb/servlet/LoginServlet.java
+++ servlet&http&request_test/src/com/hyb/servlet/LoginServlet.java
@@ -2,6 +2,7 @@ package com.hyb.servlet;
 
 import com.hyb.dao.UserDao;
 import com.hyb.domain.User;
+import org.springframework.beans.BeanUtils;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -10,40 +11,60 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet("/LoginServlet")
+
+
+@WebServlet("/loginServlet")
 public class LoginServlet extends HttpServlet {
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         //1.设置编码
-        request.setCharacterEncoding("utf-8");
+        req.setCharacterEncoding("utf-8");
         //2.获取请求参数
-        String username = request.getParameter("username");
-        String password = request.getParameter("password");
-        //3.封装User对象
+        String username = req.getParameter("username");
+        String password = req.getParameter("password");
+        //3.封装user对象
         User loginUser = new User();
         loginUser.setUsername(username);
         loginUser.setPassword(password);
+//        //2.获取所有请求参数
+//        Map<String, String[]> map = req.getParameterMap();
+//        //3.创建User对象
+//        User loginUser = new User();
+//        //3.2使用BeanUtils封装
+//        try {
+//            BeanUtils.populate(loginUser,map);
+//        } catch (IllegalAccessException e) {
+//            e.printStackTrace();
+//        } catch (InvocationTargetException e) {
+//            e.printStackTrace();
+//        }
 
-        //4.调用UserDao的Login方法
+
+        //4.调用UserDao的login方法
         UserDao dao = new UserDao();
         User user = dao.login(loginUser);
 
         //5.判断user
-        if (user == null) {
-            //登陆失败
-            request.getRequestDispatcher("/failServlet").forward(request,response);
-        }else {
-            //登录成功，存储数据
-            request.setAttribute("user",user);
+        if(user == null){
+            //登录失败
+            req.getRequestDispatcher("/failServlet").forward(req,resp);
+        }else{
+            //登录成功
+            //存储数据
+            req.setAttribute("user",user);
             //转发
             //1. 通过request对象获取请求转发器对象：RequestDispatcher getRequestDispatcher(String path)
             //2. 使用RequestDispatcher对象来进行转发：forward(ServletRequest request, ServletResponse response)
-            request.getRequestDispatcher("/successServlet").forward(request,response);
-
+            req.getRequestDispatcher("/successServlet").forward(req,resp);
         }
 
     }
 
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        this.doGet(request,response);
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        this.doGet(req,resp);
     }
 }
+
--- servlet&http&request_test/src/com/hyb/servlet/SuccessServlet.java
+++ servlet&http&request_test/src/com/hyb/servlet/SuccessServlet.java
@@ -14,11 +14,15 @@ public class SuccessServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         //获取reques域中共享的user对象
         User user = (User) request.getAttribute("user");
-        //给页面写一句话
-        //设置编码
-        response.setContentType("test/html;charset=utf-8");
-        //输出
-        response.getWriter().write("登陆失败，用户名或密码错误");
+
+        if (user != null){
+            //给页面写一句话
+            //设置编码
+            response.setContentType("test/html;charset=utf-8");
+            //输出
+            response.getWriter().write("登陆成功，"+user.getUsername()+"，欢迎您");
+        }
+
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
